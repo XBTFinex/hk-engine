36c9129ea2898da9dd1dd4e9092e82e19a982a34
Ayrton Gomes <com.ayrton@gmail.com>
2018-11-17 06:00:43 -0100

    working on stuff

--- index.js
+++ index.js
@@ -2,7 +2,3 @@ const _=require('underscore');
 const moment=require('moment');
 const fetch=require('node-fetch');
 
-fetch('http://127.0.0.1:5000/api/v1/user/me')
-.then((Data) => {
-    console.info("Data: ", Data.body);
-});
