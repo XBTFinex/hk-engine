bc9e5ece14fce61a7d5e66b355b9a165c0321068
Ayrton Gomes <com.ayrton@gmail.com>
2018-11-18 08:14:30 -0100

    working on stuff

--- boilerplat.js
+++ boilerplat.js
@@ -1,5 +1,6 @@
 var schemaData={
-    label: 'Test created_at of newly created schema #1' 
+    label: 'Test created_at of newly created schema #1',
+    picture: ''
 };
 
 fetch('http://127.0.0.1:5000/api/v1/schema', {
