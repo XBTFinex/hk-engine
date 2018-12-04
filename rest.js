4d4ad3a4fedefaefd2a5f471332cb185fae2a3d0
Ayrton Gomes <com.ayrton@gmail.com>
2018-12-04 10:07:56 -0100

    working on stuff

--- rest.js
+++ rest.js
@@ -113,16 +113,13 @@ function main(auth) {
                 console.log('[WARN] Could parse json');
                 console.log('[WARN] HTTP Response "%s"', text);
             }
+
         }, function (Err) {
             console.log('[WARN] Could parse response');
-
             console.log('[WARN] %o', Err);
         });
     }, function (Error) {
-
         console.log('[ERROR] Could make request');
-        
         console.log('[ERROR] %o', Error);
-
     });
 }
