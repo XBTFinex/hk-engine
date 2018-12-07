054569976a867d09a79c9d143f6537b4bc69e6d0
Ayrton Gomes <com.ayrton@gmail.com>
2018-12-07 11:34:46 -0100

    working on stuff

--- utils.js
+++ utils.js
@@ -19,9 +19,7 @@ function loginAndSaveToken() {
             fs.writeFile(TOKEN_FILE, JSON.stringify(auth), function(err, data){
                 if (err) {
                     console.log('[ERROR] Could not write file');
-
                     console.log('[ERROR] %o', err);
-
                     process.exit(1);
                 }
 
