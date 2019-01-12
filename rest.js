c134e933dadccceeaac7d59e2d3eb48fba05798c
Ayrton Gomes <com.ayrton@gmail.com>
2019-01-12 00:16:25 -0100

    working on stuff

--- rest.js
+++ rest.js
@@ -102,8 +102,11 @@ function main(auth) {
     console.info('%s %s', method, url);
 
     fetch(url, req)
+
     .then(function (Data){
+
         Data.text()
+
         .then(function (text) {
             try {
                 var json=JSON.parse(text);
